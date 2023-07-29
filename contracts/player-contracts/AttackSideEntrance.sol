47641bba78961486bc4b2d8279de0f7122b7952f
0x9556 <0x449556@skiff.com>
2023-07-29 16:39:21 +0800

    56

--- contracts/player-contracts/AttackSideEntrance.sol
+++ contracts/player-contracts/AttackSideEntrance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
-import {IFlashLoanEtherReceiver} from "../side-entrance/SideEntranceLenderPool.sol";
+import {IFlashLoanEtherReceiver} from "../side-entrance(solved)/SideEntranceLenderPool.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
 contract AttackSideEntrance is IFlashLoanEtherReceiver {
